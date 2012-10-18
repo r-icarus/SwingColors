58c5d33f911c62edc4e4dfc2597fa4e628009eff
Paul Hammant <paul@hammant.org>
2012-10-18 10:54:59 -0500

    more idiomatic use of PicoContainer

--- src/main/java/swingcolors/ColorComboBoxModel.java
+++ src/main/java/swingcolors/ColorComboBoxModel.java
@@ -1,24 +1,32 @@
 package swingcolors;
 
-import java.awt.Color;
-import java.util.ArrayList;
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class ColorComboBoxModel extends AbstractListModel implements ComboBoxModel {
 
     private Object selectedItem;
-    private ArrayList<NamedColor> namedColors = new ArrayList<NamedColor>();
+    private final ArrayList<NamedColor> namedColors = new ArrayList<NamedColor>();
 
-    public void addElement(Object o) {
+    public ColorComboBoxModel(String namedAndHexColors) {
+        for (String namedAndHexColor : namedAndHexColors.split("\n")) {
+            addElement(namedAndHexColor);
+        }
+    }
+
+    public ColorComboBoxModel() {
+    }
+
+    public void addElement(Object namedAndHexColor) {
         NamedColor col;
         try{
-            String values[] = o.toString().split(" ");
+            String values[] = namedAndHexColor.toString().split(" ");
             col =new NamedColor(values[0],Color.decode(values[1]));
-        }
-            catch(NumberFormatException e){
-            throw new UnsupportedOperationException(o + " not a color");
-        }catch(ArrayIndexOutOfBoundsException e){
-            throw new UnsupportedOperationException(o + " not a color");
+        } catch(NumberFormatException e){
+            throw new UnsupportedOperationException(namedAndHexColor + " not a color");
+        } catch(ArrayIndexOutOfBoundsException e){
+            throw new UnsupportedOperationException(namedAndHexColor + " not a color");
         }
         namedColors.add(col);
     }
